7e96df1f45c2f35260f2414f8bb2dada25e29368
Steven Sisavath <stevenlsisavath@gmail.com>
2022-10-31 11:57:34 -0500

    Coded method in Dinosaur class to attack robot.

--- dinosaur.py
+++ dinosaur.py
@@ -1,3 +1,4 @@
+from robot import Robot
 class Dinosaur:
 
     def __init__(self, name, attack_power):
@@ -6,4 +7,5 @@ class Dinosaur:
         self.health = 100
 
     def attack(self, robot):
-        pass
+        health = robot - self.attack_power
+        print(f'The robot has {health} health remaing!')
