a007717fb489320a3a5d908b5da18fbd7cd20576
Steven Sisavath <stevenlsisavath@gmail.com>
2022-10-31 16:26:25 -0500

    Added list of weapons in robot.py for weapon variety in active_weapon attribute.

--- robot.py
+++ robot.py
@@ -1,12 +1,18 @@
 from weapon import Weapon
+import random
 class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.active_weapon = Weapon('Lightbringer', 30)
+        self.active_weapon = random.choice(weapon_options)
 
     def attack(self, dinosaur):
         health = dinosaur - self.active_weapon.attack_power
         if health <= 0:
             health = 0
         return health
+
+lightbringer = Weapon('Lightbringer', 30)
+excalibur = Weapon('Excalibur', 40)
+harpe = Weapon('Harpe', 20)
+weapon_options = [lightbringer, excalibur, harpe]
